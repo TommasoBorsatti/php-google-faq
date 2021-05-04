b576b7324265ce41fd0f9462692ea0882172d740
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-05-04 14:54:56 +0200

    Inizializzato progetto

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<?php
+
+
+
+
+
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>google FAQ</title>
+   </head>
+   <body>
+
+    
+   </body>
+ </html>
